259a55ecc036d66a84f4345970418d7e17afd9cd
Ana <dns80791@gmail.com>
2025-03-17 20:58:21 +0800

    initial commit client

---
+++ FileConverterClient/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+
+  return (
+    <>
+    <header className='bg-fc-orange text-white h-20 text-4xl font-bold py-4 pl-16'>
+      File Converter
+    </header>
+    
+    </>      
+  )
+}
+
+export default App
